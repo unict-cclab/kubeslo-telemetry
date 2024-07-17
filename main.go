39f160396621733faa8fc47f711492ec7f9fc532
Angelo Marchese <marchese.angelo1@gmail.com>
2024-07-17 15:05:24 +0200

    Add api for requests per second

--- main.go
+++ main.go
@@ -5,8 +5,64 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/common/model"
 	"github.com/unict-cclab/kubeslo-telemetry/metrics"
 )
+
+func getAppsRequestsPerSecond(c *gin.Context) {
+	appGroupName := c.Query("app-group")
+	appName := c.Query("app")
+	rangeWidth := c.Query("range-width")
+
+	if rangeWidth == "" {
+		rangeWidth = "5m"
+	}
+
+	if appName != "" {
+		var results model.Vector
+		var err error
+
+		results, _, err = metrics.GetAppRequestsPerSecond(appGroupName, appName, rangeWidth)
+
+		//fmt.Println(warnings)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, err)
+		} else {
+			rpsValues := map[string]float64{}
+			for _, result := range results {
+				if string(result.Metric["source_app"]) == appName {
+					rpsValues[string(result.Metric["destination_app"])] = float64(result.Value)
+				} else if string(result.Metric["destination_app"]) == appName {
+					rpsValues[string(result.Metric["source_app"])] = float64(result.Value)
+				}
+			}
+			c.IndentedJSON(http.StatusOK, rpsValues)
+		}
+	} else {
+		results, _, err := metrics.GetAppsRequestsPerSecond(appGroupName, rangeWidth)
+
+		//fmt.Println(warnings)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, err)
+		} else {
+			rpsValues := map[string]map[string]float64{}
+			for _, result := range results {
+				_, ok := rpsValues[string(result.Metric["source_app"])]
+				if !ok {
+					rpsValues[string(result.Metric["source_app"])] = map[string]float64{}
+				}
+				rpsValues[string(result.Metric["source_app"])][string(result.Metric["destination_app"])] = float64(result.Value)
+
+				_, ok = rpsValues[string(result.Metric["destination_app"])]
+				if !ok {
+					rpsValues[string(result.Metric["destination_app"])] = map[string]float64{}
+				}
+				rpsValues[string(result.Metric["destination_app"])][string(result.Metric["source_app"])] = float64(result.Value)
+			}
+			c.IndentedJSON(http.StatusOK, rpsValues)
+		}
+	}
+}
 
 func getNodesLatencies(c *gin.Context) {
 	nodeName := c.Query("node")
@@ -52,6 +108,7 @@ func getNodesLatencies(c *gin.Context) {
 
 func main() {
 	router := gin.Default()
+	router.GET("/metrics/apps/rps", getAppsRequestsPerSecond)
 	router.GET("/metrics/nodes/latencies", getNodesLatencies)
 
 	err := router.Run("0.0.0.0:8080")
--- metrics/metrics.go
+++ metrics/metrics.go
@@ -23,6 +23,56 @@ func newPrometheusClient(serverAddress string) (prometheus.API, error) {
 	return prometheus.NewAPI(client), nil
 }
 
+func GetAppRequestsPerSecond(appGroupName, appName, rangeWidth string) (model.Vector, prometheus.Warnings, error) {
+	prometheusClient, err := newPrometheusClient(prometheusAddress)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to create metrics client: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	result, warnings, err := prometheusClient.Query(ctx, `
+		rate(istio_requests_total{reporter="source", app_group="`+appGroupName+`", app="`+appName+`", source_app!="unknown", destination_app!="unknown"}[`+rangeWidth+`])
+	`, time.Now())
+
+	if err != nil {
+		return nil, nil, fmt.Errorf("error during query execution: %v", err)
+	}
+
+	vector, ok := result.(model.Vector)
+
+	if !ok {
+		return nil, nil, fmt.Errorf("query result is not a vector: %v", err)
+	}
+
+	return vector, warnings, err
+}
+
+func GetAppsRequestsPerSecond(appGroupName, rangeWidth string) (model.Vector, prometheus.Warnings, error) {
+	prometheusClient, err := newPrometheusClient(prometheusAddress)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to create metrics client: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	result, warnings, err := prometheusClient.Query(ctx, `
+		rate(istio_requests_total{reporter="source", app_group="`+appGroupName+`", source_app!="unknown", destination_app!="unknown"}[`+rangeWidth+`])
+	`, time.Now())
+
+	if err != nil {
+		return nil, nil, fmt.Errorf("error during query execution: %v", err)
+	}
+
+	vector, ok := result.(model.Vector)
+
+	if !ok {
+		return nil, nil, fmt.Errorf("query result is not a vector: %v", err)
+	}
+
+	return vector, warnings, err
+}
+
 func GetNodeLatencies(nodeName, rangeWidth string) (model.Vector, prometheus.Warnings, error) {
 	prometheusClient, err := newPrometheusClient(prometheusAddress)
 	if err != nil {
